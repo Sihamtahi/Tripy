f630759c500a71f63ddf08cff54bd5ddd2259567
SIHAM TAHI <gs_tahi@esi.dz>
2021-12-29 01:03:00 +0100

    adding detail info  to the detail page

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,4 +1,7 @@
 
+import 'package:cubit_application/misc/colors.dart';
+import 'package:cubit_application/widgets/app_large_text.dart';
+import 'package:cubit_application/widgets/app_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +27,7 @@ class _DetailpageState extends State<Detailpage> {
              right: 0,
              child: Container(
                width: double.maxFinite,
-               height: 300,
+               height: 350,
                decoration: BoxDecoration(
                  image: DecorationImage(
                    image: AssetImage("img/mountain.jpeg"),
@@ -38,13 +41,62 @@ class _DetailpageState extends State<Detailpage> {
          )),
           Positioned(
               left : 20,
-              top: 70,
+              top: 50,
               child: Row(
 
                 children: [
                   IconButton(onPressed: (){},icon : Icon(Icons.menu, color: Colors.white,))
                 ],
-              ))
+              )),
+          Positioned(
+            top:320,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                  borderRadius:  BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(text: "Yosemite", color: Colors.black.withOpacity(0.8),),
+                        AppLargeText(text: "\$ 250", color: AppColors.mainColor,)
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, color: AppColors.mainColor,),
+                        SizedBox(width:5,),
+                        AppText(text: "USA, California", color: AppColors.textColor1,)
+
+
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(5, (index){
+                              return Icon(Icons.star, color: AppColors.starColor,);
+                            })
+                        ),
+                        SizedBox(width: 10),
+                        AppText(text: "(4.0)", color: AppColors.textColor2,)
+                      ],
+                    )
+                  ],
+                ),
+          ))
         ],
         ),
       ),
